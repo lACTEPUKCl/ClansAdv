f667a3ac3ac24d5e42c66b0c4a54a2e489de6ce8
ACTEPUKC <makakakakala@gmail.com>
2024-08-05 02:44:40 +0300

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import schedule from "node-schedule";
 import { config } from "dotenv";
 config();
 
-async function sendMessage() {
+async function sendMessage(content) {
   const client = new Client({
     intents: [
       GatewayIntentBits.GuildMessages,
@@ -16,7 +16,18 @@ async function sendMessage() {
 
     const channel = await client.channels.fetch(process.env.CHANNEL_ID);
     if (channel) {
-      await channel.send(`Сообщество **SKAT** готово рассмотреть кандидатов для совместных игр в Squad.
+      await channel.send(content);
+    } else {
+      console.error("Канал не найден.");
+    }
+
+    client.destroy();
+  });
+
+  await client.login(process.env.DISCORD_TOKEN);
+}
+
+const message1 = `Сообщество **SKAT** готово рассмотреть кандидатов для совместных игр в Squad.
 
 [x][Активным членам сообщества предоставляем **VIP** пропуска на **РНС** сервера]
 [x][Проводим клановые тренировки]
@@ -36,17 +47,38 @@ async function sendMessage() {
 
 **[Также активно играем в различные проекты]**
 https://discord.gg/ndm4fPFTpB
-https://cdn.discordapp.com/attachments/765988405791555635/1264939650561081464/0423d4beee19884c.png?ex=66af845a&is=66ae32da&hm=194e223294473a128cbb4fc13d1d1219a69ecec69f189d57fc91780afba62e34&`);
-    } else {
-      console.error("Канал не найден.");
-    }
+https://cdn.discordapp.com/attachments/765988405791555635/1264939650561081464/0423d4beee19884c.png?ex=66af845a&is=66ae32da&hm=194e223294473a128cbb4fc13d1d1219a69ecec69f189d57fc91780afba62e34&`;
 
-    client.destroy();
-  });
+const message2 = `Стоп
+**Игровое сообщество 5.45 проводит набор кандидатов на вступление.**
 
-  await client.login(process.env.DISCORD_TOKEN);
-}
+Наше сообщество создано для комфортной и веселой игры. У нас нет токсичного общения, только позитивный вайб.
 
+— Проводятся мероприятия по разнообразию игры в SQUAD;
+— Активные участники клана имеют возможность получить клановую VIP;
+— Помимо SQUAD сможете найти себе напарников и в другие игры;
+— Участвуем в CW и разных ивентах.
+
+**Мы готовы принять вас, если:**
+
+—  Ваш возраст равен или более 18 лет (возможны исключения);
+—  Вам нравится играть в команде, получать новый опыт, проводить работу над ошибками в своих играх;
+—  Вы воспринимаете адекватную критику в свою сторону;
+—  Адекватность и отзывчивость - это про вас.
+
+Основной "прайм тайм" клана в будние дни с 19:00 до 24:00 по МСК. 
+
+**Если вы заинтересованы, рады будем ответить на ваши вопросы**
+
+https://discord.gg/Q4Rsa8zKQ3
+https://cdn.discordapp.com/attachments/1135615323563905086/1269716491801202729/IMG_8181.PNG?ex=66b11321&is=66afc1a1&hm=2fb3eb01a913114b9143e35703aeb99d5c4af7379bd51a69b78bdcd456c154aa&`;
+
+// Schedule the first message at 10:00 AM
 schedule.scheduleJob("0 10 * * *", () => {
-  sendMessage();
+  sendMessage(message1);
+});
+
+// Schedule the second message at 5:00 PM
+schedule.scheduleJob("0 17 * * *", () => {
+  sendMessage(message2);
 });
